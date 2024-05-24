88c16e792b10d77f035ba5bbcc0c4a785fbe33b0
Haris Mohanty <harismohanty8658@gmail.com>
2024-05-24 10:51:30 +0530

    server- Login user routes and controller function created successfully.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import validator from "validator";
 import UserModel from "../Models/UserModel.js";
 
@@ -71,7 +72,73 @@ export const registerUser = async (req, res) => {
     return res.status(201).json({
       success: true,
       message: "User registered successfully!",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error!",
+      error: err.message,
+    });
+  }
+};
+
+// ******************** LOGIN USER ***************/
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email || !password) {
+      return res.status(422).json({
+        success: false,
+        message: "Please provide all fields!",
+      });
+    }
+
+    // Email Validation
+    if (!validator.isEmail(email)) {
+      return res.status(422).json({
+        success: false,
+        message: "Please provide a valid email address!",
+      });
+    }
+
+    // Password validation
+    if (!validator.isLength(password, { min: 6 })) {
+      return res.status(422).json({
+        success: false,
+        message: "Password must be at least 6 characters long!",
+      });
+    }
+
+    // Get the user
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    // Password Compare
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect Password, Please check again...",
+      });
+    }
+
+    // Generate Token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    // Login Success
+    return res.status(200).json({
+      success: true,
       user,
+      token
     });
   } catch (err) {
     return res.status(500).json({
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { loginUser, registerUser } from "../controllers/userController.js";
 
 // Router Obj
 const router = express.Router();
 
 // Register user
 router.post("/register", registerUser);
+
+// Login user
+router.post("/login", loginUser);
 
 //Export
 export default router;
