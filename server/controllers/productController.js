d2126f8b52ab23625540a5f11cb7b64f6932cc37
Haris Mohanty <harismohanty8658@gmail.com>
2024-06-03 22:07:17 +0530

    server- Create routes and controller function for update product successfully.

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -119,3 +119,112 @@ export const getAllProductsController = async (req, res) => {
     });
   }
 };
+
+//**************** UPDATE PRODUCT DETAILS CONTROLLER ***********/
+export const updateProductController = async (req, res) => {
+  try {
+    // Check the user is admin or not
+    const userId = req.user;
+    const user = await UserModel.findById(userId);
+    if (!user || !user.isAdmin) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied. Admins only.",
+      });
+    }
+
+    // Get the data from client
+    const {
+      productName,
+      brandName,
+      category,
+      productImage,
+      price,
+      sellingPrice,
+      description,
+    } = req.body;
+
+    // Validation
+    if (
+      !productName ||
+      !brandName ||
+      !category ||
+      !price ||
+      !sellingPrice ||
+      !description
+    ) {
+      return res.status(422).json({
+        success: false,
+        message: "All fields are required!",
+      });
+    }
+
+    // Validation of product image
+    if (!Array.isArray(productImage) || productImage.length === 0) {
+      return res.status(422).json({
+        success: false,
+        message: "At least one product image is required",
+      });
+    }
+
+    //Validation of product price
+    if (!validator.isFloat(price.toString(), { gt: 0 })) {
+      return res.status(400).json({
+        success: false,
+        message: "Price must be a positive number",
+      });
+    }
+
+    //Validation of product selling price
+    if (!validator.isFloat(sellingPrice.toString(), { gt: 0 })) {
+      return res.status(400).json({
+        success: false,
+        message: "Selling price must be a positive number",
+      });
+    }
+
+    // Selling price is less than actual price
+    if (parseFloat(sellingPrice) > parseFloat(price)) {
+      return res.status(400).json({
+        success: false,
+        message: "Selling price cannot be higher than price",
+      });
+    }
+
+    // Update Product Details
+    const updateProductDetails = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        productName,
+        brandName,
+        category,
+        productImage,
+        price,
+        sellingPrice,
+        description,
+      },
+      { new: true }
+    );
+
+    // Check the product is exists or not
+    if (!updateProductDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found!",
+      });
+    }
+
+    //Success Response
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully!",
+      updateProductDetails,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error!",
+      error: err.message,
+    });
+  }
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,6 +3,7 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 import {
   uploadProductController,
   getAllProductsController,
+  updateProductController,
 } from "../controllers/productController.js";
 
 //Router Obj
@@ -14,6 +15,9 @@ router.post("/upload-product", authMiddleware, uploadProductController);
 
 // Get all product
 router.get("/get-all-products", getAllProductsController);
+
+// Update Product details
+router.put("/update-product/:id", authMiddleware, updateProductController);
 
 //Export
 export default router;
