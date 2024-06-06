7d0c75c6b63d61571e52f655fc76bb925ce89b06
Haris Mohanty <harismohanty8658@gmail.com>
2024-06-06 11:55:04 +0530

    server- Routes created for get product category.

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,6 +4,7 @@ import {
   uploadProductController,
   getAllProductsController,
   updateProductController,
+  getProductCategoryController,
 } from "../controllers/productController.js";
 
 //Router Obj
@@ -18,6 +19,9 @@ router.get("/get-all-products", getAllProductsController);
 
 // Update Product details
 router.put("/update-product/:id", authMiddleware, updateProductController);
+
+// Get Product Category
+router.get("/get-product-category", getProductCategoryController);
 
 //Export
 export default router;
