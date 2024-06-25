37defd4d6e653122cce3cca722752e3ad8c24d11
Haris Mohanty <harismohanty8658@gmail.com>
2024-06-25 22:50:27 +0530

    client- Footer section created.

--- client/src/Pages/MyAddress.jsx
+++ client/src/Pages/MyAddress.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "../redux/spinnerSlice";
+import { getAddresses } from "../api/api";
 
 const MyAddress = () => {
+  const dispatch = useDispatch();
+  const [addresses, setAddresses] = useState([]);
+
+  //************ FETCH CREATED ADDRESSES **********/
+  const fetchCreatedAddresses = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await getAddresses();
+      dispatch(hideLoading());
+      if (res.success) {
+        setAddresses(res.data);
+      }
+    } catch (err) {
+      dispatch(hideLoading());
+      console.log(err?.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchCreatedAddresses();
+    //eslint-disable-next-line
+  }, []);
+
+  console.log(addresses);
   return (
     <>
       <div className="container mx-auto mt-16">My Addresses</div>
--- client/src/Pages/MyOrders.jsx
+++ client/src/Pages/MyOrders.jsx
@@ -34,7 +34,6 @@ const MyOrders = () => {
       }
     } catch (err) {
       dispatch(hideLoading());
-      toast.error(err?.response?.data?.message);
     }
   };
   //************ CANCEL ORDERS ********/
@@ -141,8 +140,6 @@ const MyOrders = () => {
     navigate("/cart");
   };
 
-  console.log(orders);
-
   return (
     <div className="container mx-auto mt-16">
       <div className="bg-white p-4 md:p-8 rounded-lg shadow-lg">
@@ -152,155 +149,170 @@ const MyOrders = () => {
           similar products.
         </p>
 
-        {orders.map((order) => (
-          <div key={order._id} className="mb-10">
-            <div className="border border-gray-300 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
-              <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4">
-                <div className="mb-2 sm:mb-0 md:text-center">
-                  <p className="text-lg font-medium text-gray-900">
-                    Order number
-                  </p>
-                  <p className="text-primary-600 font-semibold">
-                    {order?.paymentId ? order.paymentId : order._id}
-                  </p>
-                </div>
-                <div className="mb-2 sm:mb-0 md:text-center">
-                  <p className="text-lg font-medium text-gray-900">
-                    Date placed
-                  </p>
-                  <p className="text-primary-600 font-semibold">
-                    {new Date(order.createdAt).toLocaleDateString()}
-                  </p>
-                </div>
-                <div className="mb-2 sm:mb-0 md:text-center">
-                  <p className="text-lg font-medium text-gray-900">
-                    Total amount
-                  </p>
-                  <p className="text-indigo-600 font-semibold">
-                    {displayInr(order.amount)}{" "}
-                    <span className="text-gray-900 text-xs">(2% GST)</span>
-                  </p>
-                </div>
-                <div className="mb-2 sm:mb-0 md:text-center">
-                  <p className="text-lg font-medium text-gray-900">
-                    Payment Status
-                  </p>
-                  <p className="text-green-600 font-semibold">
-                    {order?.paymentStatus === "Authorized"
-                      ? "COD"
-                      : order.paymentStatus}
-                  </p>
-                </div>
-                <div className="flex space-x-2">
-                  <button
-                    type="button"
-                    onClick={() => cancelOrder(order?._id)}
-                    className="bg-red-700 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-red-800"
-                  >
-                    Cancel Order
-                  </button>
-                  {order.paymentStatus === "Paid" ? (
-                    <button
-                      onClick={() => handleShowInvoice(order)}
-                      className="bg-indigo-700 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-indigo-800"
-                    >
-                      Show Invoice
-                    </button>
-                  ) : (
+        {orders.length === 0 ? (
+          <div className="flex flex-col items-center justify-center min-h-[40vh] text-center">
+            <h2 className="text-2xl font-semibold mb-2">No orders found</h2>
+            <p className="mb-4 text-gray-500 px-4">
+              Looks like you haven't ordered anything.
+            </p>
+            <Link
+              to="/"
+              className="bg-indigo-500 text-white px-6 py-3 rounded-full shadow-md hover:shadow-lg transition-transform transform hover:scale-105"
+            >
+              Shop Now
+            </Link>
+          </div>
+        ) : (
+          orders.map((order) => (
+            <div key={order._id} className="mb-10">
+              <div className="border border-gray-300 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
+                <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4">
+                  <div className="mb-2 sm:mb-0 md:text-center">
+                    <p className="text-lg font-medium text-gray-900">
+                      Order number
+                    </p>
+                    <p className="text-primary-600 font-semibold">
+                      {order?.paymentId ? order.paymentId : order._id}
+                    </p>
+                  </div>
+                  <div className="mb-2 sm:mb-0 md:text-center">
+                    <p className="text-lg font-medium text-gray-900">
+                      Date placed
+                    </p>
+                    <p className="text-primary-600 font-semibold">
+                      {new Date(order.createdAt).toLocaleDateString()}
+                    </p>
+                  </div>
+                  <div className="mb-2 sm:mb-0 md:text-center">
+                    <p className="text-lg font-medium text-gray-900">
+                      Total amount
+                    </p>
+                    <p className="text-indigo-600 font-semibold">
+                      {displayInr(order.amount)}{" "}
+                      <span className="text-gray-900 text-xs">(2% GST)</span>
+                    </p>
+                  </div>
+                  <div className="mb-2 sm:mb-0 md:text-center">
+                    <p className="text-lg font-medium text-gray-900">
+                      Payment Status
+                    </p>
+                    <p className="text-green-600 font-semibold">
+                      {order?.paymentStatus === "Authorized"
+                        ? "COD"
+                        : order.paymentStatus}
+                    </p>
+                  </div>
+                  <div className="flex space-x-2">
                     <button
                       type="button"
-                      onClick={() => payNowHandler(order)}
-                      className="bg-green-700 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-green-800"
+                      onClick={() => cancelOrder(order?._id)}
+                      className="bg-red-700 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-red-800"
                     >
-                      Pay Now
+                      Cancel Order
                     </button>
-                  )}
-                </div>
-              </div>
-
-              <div className="mt-6">
-                {order.products.map((product) => (
-                  <div
-                    key={product._id}
-                    className="flex flex-col sm:flex-row border-t border-gray-300 py-4 items-start mt-4"
-                  >
-                    {product.productId.productImage &&
-                      product.productId.productImage.length > 0 && (
-                        <img
-                          src={product.productId.productImage[0]}
-                          alt={product.productId.productName}
-                          className="w-32 md:w-44 h-32 md:h-44 object-scale-down mix-blend-multiply"
-                        />
-                      )}
-                    <div className="ml-0 sm:ml-6 mt-2 sm:mt-0 flex-grow">
-                      <h3 className="text-xl font-semibold text-gray-900 text-ellipsis line-clamp-1">
-                        {product.productId.productName}
-                      </h3>
-                      <p className="text-gray-500 mt-2 text-ellipsis line-clamp-2">
-                        {product.productId.description}
-                      </p>
-                      <div className="flex space-x-2">
-                        <p className="text-2xl font-bold text-slate-900">
-                          {displayInr(product.productId.sellingPrice)}
-                        </p>
-                        <p className="text-sm text-slate-900 line-through mt-3">
-                          {displayInr(product.productId.price)}
-                        </p>
-                        <p className="text-sm font-bold text-indigo-700 mt-2">
-                          {Math.round(
-                            ((product.productId.price -
-                              product.productId.sellingPrice) /
-                              product.productId.price) *
-                              100
-                          )}
-                          % OFF
-                        </p>
-                      </div>
-                      <div className="flex justify-start sm:items-end space-x-2 space-y-0 md:space-y-4 sm:mt-0">
-                        <Link
-                          to={`/product-details/${product?.productId._id}`}
-                          className="bg-blue-500 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-blue-600"
-                        >
-                          View Product
-                        </Link>
-                        <button
-                          type="button"
-                          onClick={() =>
-                            handleBuyNow(product?.productId._id, dispatch)
-                          }
-                          className="bg-green-500 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-green-600"
-                        >
-                          Buy Again
-                        </button>
-                      </div>
-                      <p className="flex justify-end text-sm font-semibold text-indigo-900 mt-2  md:-mt-8">
-                        Quantity: {product.quantity}
-                      </p>
-                    </div>
+                    {order.paymentStatus === "Paid" ? (
+                      <button
+                        onClick={() => handleShowInvoice(order)}
+                        className="bg-indigo-700 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-indigo-800"
+                      >
+                        Show Invoice
+                      </button>
+                    ) : (
+                      <button
+                        type="button"
+                        onClick={() => payNowHandler(order)}
+                        className="bg-green-700 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-green-800"
+                      >
+                        Pay Now
+                      </button>
+                    )}
                   </div>
-                ))}
-              </div>
+                </div>
 
-              <ProgressBar currentStatus={order.orderStatus} />
-              <div className="mt-4">
-                <h4 className="text-lg font-medium text-gray-900">
-                  Shipping Address
-                </h4>
-                <div className="md:flex md:gap-2 md:mt-2 text-gray-600">
-                  <p>
-                    {order.addressId.houseNo}, {order.addressId.area},{" "}
-                    {order.addressId.landmark}
-                  </p>
-                  <p>
-                    {order.addressId.district}, {order.addressId.state},{" "}
-                    {order.addressId.country}, {order.addressId.postalCode}
-                  </p>
-                  <p>Phone: {order.addressId.phoneNumber}</p>
+                <div className="mt-6">
+                  {order.products.map((product) => (
+                    <div
+                      key={product._id}
+                      className="flex flex-col sm:flex-row border-t border-gray-300 py-4 items-start mt-4"
+                    >
+                      {product.productId.productImage &&
+                        product.productId.productImage.length > 0 && (
+                          <img
+                            src={product.productId.productImage[0]}
+                            alt={product.productId.productName}
+                            className="w-32 md:w-44 h-32 md:h-44 object-scale-down mix-blend-multiply"
+                          />
+                        )}
+                      <div className="ml-0 sm:ml-6 mt-2 sm:mt-0 flex-grow">
+                        <h3 className="text-xl font-semibold text-gray-900 text-ellipsis line-clamp-1">
+                          {product.productId.productName}
+                        </h3>
+                        <p className="text-gray-500 mt-2 text-ellipsis line-clamp-2">
+                          {product.productId.description}
+                        </p>
+                        <div className="flex space-x-2">
+                          <p className="text-2xl font-bold text-slate-900">
+                            {displayInr(product.productId.sellingPrice)}
+                          </p>
+                          <p className="text-sm text-slate-900 line-through mt-3">
+                            {displayInr(product.productId.price)}
+                          </p>
+                          <p className="text-sm font-bold text-indigo-700 mt-2">
+                            {Math.round(
+                              ((product.productId.price -
+                                product.productId.sellingPrice) /
+                                product.productId.price) *
+                                100
+                            )}
+                            % OFF
+                          </p>
+                        </div>
+                        <div className="flex justify-start sm:items-end space-x-2 space-y-0 md:space-y-4 sm:mt-0">
+                          <Link
+                            to={`/product-details/${product?.productId._id}`}
+                            className="bg-blue-500 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-blue-600"
+                          >
+                            View Product
+                          </Link>
+                          <button
+                            type="button"
+                            onClick={() =>
+                              handleBuyNow(product?.productId._id, dispatch)
+                            }
+                            className="bg-green-500 text-white px-4 py-2 rounded-md transition duration-200 ease-in-out hover:bg-green-600"
+                          >
+                            Buy Again
+                          </button>
+                        </div>
+                        <p className="flex justify-end text-sm font-semibold text-indigo-900 mt-2  md:-mt-8">
+                          Quantity: {product.quantity}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <ProgressBar currentStatus={order.orderStatus} />
+                <div className="mt-4">
+                  <h4 className="text-lg font-medium text-gray-900">
+                    Shipping Address
+                  </h4>
+                  <div className="md:flex md:gap-2 md:mt-2 text-gray-600">
+                    <p>
+                      {order.addressId.houseNo}, {order.addressId.area},{" "}
+                      {order.addressId.landmark}
+                    </p>
+                    <p>
+                      {order.addressId.district}, {order.addressId.state},{" "}
+                      {order.addressId.country}, {order.addressId.postalCode}
+                    </p>
+                    <p>Phone: {order.addressId.phoneNumber}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
 
       {/* Invoice Modal */}
